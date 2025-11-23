ca226582620ec8883019c0b67d015b1704961520
Sthembizo Molefi <sthembizomolefi@gmail.com>
2025-11-23 16:55:57 +0200

    initial setUp

---
+++ backend/src/main/java/com/medivault/backend/MediVaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medivault.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediVaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediVaultBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/medivault/backend/MediVaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medivault.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediVaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
